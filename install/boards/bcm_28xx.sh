dbc84a4892ce0e8bc7d8e94d41010594ee91806d
Patrick José Pereira <patrickelectric@gmail.com>
2021-10-28 16:14:34 -0300

    remove sudo
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- install/boards/bcm_28xx.sh
+++ install/boards/bcm_28xx.sh
@@ -5,10 +5,10 @@ echo "Configuring BCM28XX board (Raspberry Pi zero, 1, 2, 3).."
 # Remove any configuration related to i2c and spi/spi1 and do the necessary changes for navigator
 echo "- Enable I2C, SPI and UART."
 for STRING in "dtparam=i2c_arm=" "dtparam=spi=" "dtoverlay=spi1" "dtoverlay=uart1"; do
-    sudo sed -i "/$STRING/d" /boot/config.txt
+    sed -i "/$STRING/d" /boot/config.txt
 done
 for STRING in "dtparam=i2c_arm=on" "dtparam=spi=on" "dtoverlay=spi1-3cs" "dtoverlay=uart1"; do
-    echo "$STRING" | sudo tee -a /boot/config.txt
+    echo "$STRING" | tee -a /boot/config.txt
 done
 
 # Check for valid modules file to load kernel modules
@@ -22,10 +22,10 @@ fi
 echo "- Set up kernel modules."
 # Remove any configuration or commented part related to the i2c drive
 for STRING in "bcm2835-v4l2" "i2c-bcm2835" "i2c-dev"; do
-    sudo sed -i "/$STRING/d" "$MODULES_FILE"
-    echo "$STRING" | sudo tee -a "$MODULES_FILE"
+    sed -i "/$STRING/d" "$MODULES_FILE"
+    echo "$STRING" | tee -a "$MODULES_FILE"
 done
 
 # Remove any console serial configuration
 echo "- Configure serial."
-sudo sed -e 's/console=serial[0-9],[0-9]*\ //' -i /boot/cmdline.txt
+sed -e 's/console=serial[0-9],[0-9]*\ //' -i /boot/cmdline.txt
