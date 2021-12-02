40d5d67308ad5bb347d0c393679628de45f439e5
Patrick José Pereira <patrickelectric@gmail.com>
2021-12-02 16:21:51 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- deploy/initial-setup/build.sh
+++ deploy/initial-setup/build.sh
@@ -1,11 +1,13 @@
 cd linux
 
 KERNEL=kernel7l
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2711_navigator_defconfig
-make -j24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
-sudo env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=/rootfs modules_install
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- bcm2711_navigator_defconfig
+make -j24 ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- zImage modules dtbs
+sudo env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- INSTALL_MOD_PATH=/rootfs modules_install
 sudo cp arch/arm/boot/zImage boot/$KERNEL.img
 sudo cp arch/arm/boot/dts/*.dtb boot/
 sudo mkdir -p boot/overlays
 sudo cp arch/arm/boot/dts/overlays/*.dtb* boot/overlays/
 sudo cp arch/arm/boot/dts/overlays/README boot/overlays/
+# Enable SSH
+sudo touch boot/ssh
--- deploy/initial-setup/setup.sh
+++ deploy/initial-setup/setup.sh
@@ -3,17 +3,22 @@ FILENAME=$(basename $FILE)
 wget -nc https://downloads.raspberrypi.org/raspios_lite_armhf/images/$FILE
 unzip -n $FILENAME
 
-git clone --depth 1  --branch rpi-4.19.y git@github.com:Williangalvani/linux.git
+git clone --depth 1  --branch rpi-4.19.y https://github.com/Williangalvani/linux
 
-# umount if previously mounted
-sudo umount /dev/loop0p1
 sudo partx -a -v *.img
 # create mountpoints and mount image
 mkdir linux/root
 mkdir linux/boot
-sudo mount /dev/loop0p1 linux/boot
-#mount /dev/loop1 linux/root
 
-sh build.sh
+# Get first partition (boot) of loop device
+PARTITION=$(ls /dev/loop*  | tail -n 2 | head -n 1)
+sudo mount $PARTITION linux/boot
+
+echo "Starting build!"
+build.sh
+ls linux/boot
+echo "Build done!"
 sync
-sudo umount /dev/loop0p1
+sudo umount $PARTITION --detach-loop
+sync
+echo "done!"
