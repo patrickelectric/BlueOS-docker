cc2fd12390b1f146d927d97e595af1e0ab15fb24
Patrick José Pereira <patrickelectric@gmail.com>
2022-03-25 17:52:38 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- install/install.sh
+++ install/install.sh
@@ -163,8 +163,8 @@ sed -i '/noipv4ll/d' /etc/dhcpcd.conf
 sed -i '$ a noipv4ll' /etc/dhcpcd.conf
 
 echo "Downloading bootstrap"
-BLUEOS_BOOTSTRAP="bluerobotics/blueos-bootstrap:$VERSION" # Use current version
-BLUEOS_CORE="bluerobotics/blueos-core:$VERSION" # We don't have a stable tag yet
+BLUEOS_BOOTSTRAP="bluerobotics/blueos-bootstrap:master" # Use current version
+BLUEOS_CORE="bluerobotics/blueos-core:master" # We don't have a stable tag yet
 BLUEOS_FACTORY="bluerobotics/blueos-core:factory" # used for "factory reset"
 
 docker pull $BLUEOS_BOOTSTRAP
